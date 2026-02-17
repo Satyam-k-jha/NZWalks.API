f6562b15f2f3d00ee5ddc2eb6ecdc0be24074f0c
Satyam-k-jha <skumar65321@gmail.com>
2026-02-18 03:43:41 +0530

    first Commit

---
+++ Controllers/RegionsController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Models.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext _context;
+        private readonly IRegionRepository regionRepository;
+        public RegionsController(NZWalksDbContext dbContext, IRegionRepository regionRepository)
+        {
+            _context = dbContext;
+            this.regionRepository = regionRepository; 
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+           var regions = await regionRepository.GetAllAsync();
+            var regionDto = new List<RegionDto>();
+            foreach (var region in regions)
+            {
+                regionDto.Add(new RegionDto()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageUrl = region.RegionImageUrl
+                });
+            }
+            return Ok(regions);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            var region = await regionRepository.GetByIdAsync(id);
+
+            
+            if(region == null)
+            {
+                return NotFound();
+            }
+            var regionDto = new RegionDto()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+            return Ok(regionDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            var region = new Region()
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+            region = await regionRepository.CreateAsync(region);
+            var regionDto = new RegionDto()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+            return CreatedAtAction(nameof(GetById), new {id = regionDto.Id}, regionDto);
+        }
+
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegion)
+        {
+            var region = new Region()
+            {
+                Code = updateRegion.Code,
+                Name = updateRegion.Name,
+                RegionImageUrl = updateRegion.RegionImageUrl
+            };
+
+
+            region = await regionRepository.UpdateAsync(id, region);
+            if(region == null)
+            {
+                return NotFound();
+            }
+
+            var regionDto = new RegionDto()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+            return Ok(regionDto);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var region = await regionRepository.DeleteAsync(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            var regionDto = new RegionDto()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+            return Ok(regionDto);
+        }
+    }
+}
---
+++ Models/Repositories/SQLRegionRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Models.Repositories
+{
+    public class SQLRegionRepository : IRegionRepository
+    {
+        public readonly NZWalksDbContext _context;
+        public SQLRegionRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            _context = nZWalksDbContext;
+        }
+
+        public async Task<Region> CreateAsync(Region region)
+        {
+            await _context.Regions.AddAsync(region);
+            await _context.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region?> DeleteAsync(Guid id)
+        {
+            var existingRegion = await _context.Regions.FindAsync(id);
+            if (existingRegion == null)
+            {
+                return null;
+            }
+            _context.Regions.Remove(existingRegion);
+            await _context.SaveChangesAsync();
+            return existingRegion;
+        }
+
+        public async Task<List<Region>> GetAllAsync()
+        {
+            return await _context.Regions.ToListAsync();
+        }
+
+        public async Task<Region?> GetByIdAsync(Guid id)
+        {
+            return await _context.Regions.FirstOrDefaultAsync(x => x.Id==id);
+        }
+
+        public async Task<Region?> UpdateAsync(Guid id, Region region)
+        {
+            var existingRegion = await _context.Regions.FindAsync(id);
+            if (existingRegion == null)
+            {
+                return null;
+            }
+            existingRegion.Code = region.Code;
+            existingRegion.Name = region.Name;
+            existingRegion.RegionImageUrl = region.RegionImageUrl;
+
+            await _context.SaveChangesAsync();
+            return region;
+        }
+    }
+}
